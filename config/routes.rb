f18e3992f059ff0a6b33d234a60c9f50331f02fb
Daichi Itoh <daich128tech@gmail.com>
2020-11-16 20:29:34 +0900

    インデントの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#index'
-	get 'home/about' => 'homes#about'
+  get 'home/about' => 'homes#about'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
